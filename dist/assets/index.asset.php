931371fe10ff8ef2e86572e650db94d5f2b04a4a
Puleeno Nguyen <puleeno@gmail.com>
2026-06-09 22:54:41 +0700

    chore: update asset version hashes for svg-data-map frontend and index files

--- dist/assets/index.asset.php
+++ dist/assets/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-components', 'wp-i18n', 'wp-block-editor'), 'version' => '1781019861989');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-components', 'wp-i18n', 'wp-block-editor'), 'version' => '1781020366496');
